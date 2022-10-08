56781451d634af540d86d6450e6b3fd75129b63b
Manel <ea3eiz@ea3eiz.com>
2022-10-08 13:13:53 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Actualiza  macbook
                         cd /home/pi
-                        sudo rm -R B108
-                        git clone http://github.com/ea3eiz/EMISORA
+                        sudo rm -R D108
+                        git clone http://github.com/ea3eiz/D108
                         sleep 1
-                        mv EMISORA B108
+                        mv D108 B108
                         sleep 1
-                        sudo chmod 777 -R B108
-                        cd /home/pi/B108
+                        sudo chmod 777 -R D108
+                        cd /home/pi/D108
                         ./qt_imagen_actualizada
 

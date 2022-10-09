8c85bfdab081472ce4fc599e52add57fe70431f0
Manel <ea3eiz@ea3eiz.com>
2022-10-09 19:35:31 +0200

    1

--- activar_dvswitch.sh
+++ activar_dvswitch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./qt_dvswitch_activado &
+
 sed -i "18c DVSWITCH=ON" /home/pi/status.ini
 
 sudo systemctl restart ysfgateway.service
@@ -12,7 +14,7 @@ sudo systemctl restart nxdngateway.service
 sleep 1
 sudo systemctl restart p25gateway.service
 cd /home/pi/B108
-./qt_dvswitch_activado &
+
 
 # Directorio donde están todos los servicios
 #/usr/lib/systemd/system
--- desactivar_dvswitch.sh
+++ desactivar_dvswitch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./qt_dvswitch_desactivado &
+
 sed -i "18c DVSWITCH=OFF" /home/pi/status.ini
 
 sudo systemctl stop ysfgateway.service
@@ -12,7 +14,7 @@ sudo systemctl stop nxdngateway.service
 sleep 1
 sudo systemctl restart p25gateway.service
 cd /home/pi/B108
-./qt_dvswitch_desactivado &
+
 
 # Directorio donde están todos los servicios
 #/usr/lib/systemd/system

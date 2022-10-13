3d22941d4f4eabfef07b56ee67abe337ec704a5b
Manel <ea3eiz@ea3eiz.com>
2022-10-13 20:17:46 +0200

    1

--- cerrar_dv4mini.sh
+++ cerrar_dv4mini.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo su | ps aux | grep dv4mini.exe | grep -v grep | awk '{print $2}' | xargs kill
+sudo su
+ps aux | grep dv4mini.exe | grep -v grep | awk '{print $2}' | xargs kill
 sudo killall dv_serial
+
 
 

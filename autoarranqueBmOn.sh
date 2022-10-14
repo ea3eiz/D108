ce604d4116e2565ea8e54857baf66edafb79bd6b
Manel <ea3eiz@ea3eiz.com>
2022-10-14 19:20:29 +0200

    1

--- autoarranqueBmOn.sh
+++ autoarranqueBmOn.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-sed -i '8c BM=OFF' /home/pi/.local/autoarranque.ini
-sudo cp /home/pi/AUTOARRANQUE_B108/BRANDMEISTER.desktop /home/pi/.config/autostart
+sed -i '8c BM=ON' /home/pi/.local/autoarranque.ini
+cp /home/pi/AUTOARRANQUE_B108/BRANDMEISTER.desktop /home/pi/.config/autostart

b766f2a451d30841a619c73140da780faa68369a
Manel <ea3eiz@ea3eiz.com>
2022-10-15 16:43:54 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -2,4 +2,4 @@
 sudo killall -9 MMDVMBM
 sudo sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi/MMDVMHost
-xterm -geometry 73x8+556+0 -bg brown -fg white -fa ‘verdana’ -fs 8x -T CONSOLA_BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini
+xterm -geometry 73x9+556+0 -bg brown -fg white -fa ‘verdana’ -fs 8x -T CONSOLA_BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini

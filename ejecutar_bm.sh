6dfcc2f60fcc8442deb39af0dcd298cc7c2002a3
Manel <ea3eiz@ea3eiz.com>
2022-10-29 11:48:10 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -5,4 +5,3 @@ frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 sed -i "13c $frecuencia" /home/pi/INFO_RXF.ini
 cd /home/pi/MMDVMHost
 xterm -geometry 73x10+290+96 -bg brown -fg white -fa ‘verdana’ -fs 8x -T CONSOLA_BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini
-RXFrequency=437000000

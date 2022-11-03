c9cb5cc33c99a3687d555245f68704a83a5daf5b
Manel <ea3eiz@ea3eiz.com>
2022-11-03 11:20:22 +0100

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -6,7 +6,7 @@ baut_rate=$(awk "NR==3" /home/pi/.local/ambe_server.ini)
 cd /home/pi/AMBE_SERVER
 sudo killall -9 AMBEserver
 sleep 1
-xterm -geometry 73x10+1387+320 -bg black -fg white -fa ‘verdana’ -fs 8x -T AMBE_SERVER -e sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
+xterm -geometry 73x10+1200+320 -bg black -fg white -fa ‘verdana’ -fs 10x -T AMBE_SERVER -e sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
 
 #./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
 
